9b387771e3bbf1345ade2ec751ab825f6f61e6f8
2402zmybie <2402368990@qq.com>
2020-07-05 19:04:34 +0800

    spring aop之使用注解配置

---
+++ src/test/java/com/hr/text/AopTest.java
@@ -0,0 +1,16 @@
+package com.hr.text;
+
+
+import com.hr.service.IAccountService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AopTest {
+
+    public static void main(String[] args) {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        IAccountService iAccountService = ac.getBean("accountService",IAccountService.class);
+
+        iAccountService.saveAccount();
+    }
+}
